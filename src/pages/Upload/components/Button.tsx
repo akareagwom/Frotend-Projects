73ceb4ca7aa2d1f0d8892af6e487e4fc764d208f
akareagwom <akareazikang20@gmail.com>
2025-11-24 21:57:38 +0100

    upload section

---
+++ src/pages/Upload/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div className='bg-blue-500 rounded-sm'>
+      Button
+    </div>
+  )
+}
+
+export default Button
---
+++ src/pages/Upload/upload.tsx
@@ -0,0 +1,23 @@
+import { FaFile } from "react-icons/fa6";
+import Button from './components/Button'
+
+const Upload =()=>{
+    return(
+        <div className="bg-white">
+            <div className="flex">
+                <div className="bg-gray-200 flex flex-col justify-center items-center rounded-sm">
+                <div className="text-blue-400 text-6xl">
+                    <FaFile />
+                    <input type="file" name="" id="" />
+                </div>
+                    <h1>Upload your file</h1>
+                    <p>Drag and drop a file or browse</p>
+                    <Button/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Upload; 
