96a08c5b1352db283f7321e3f1a4519840d580a1
akareagwom <akareazikang20@gmail.com>
2025-11-27 21:56:05 +0100

    custom button

--- src/pages/Upload/components/Button.tsx
+++ src/pages/Upload/components/Button.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 
-function Button() {
+type Variant="primary"|"secondary";
+type Size = "sm" | "md" | "lg";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: Variant;
+  size?: Size;
+  text?: string;
+}
+
+const variantStyles: Record<Variant, string> = {
+  primary: "bg-blue-600 text-white hover:bg-blue-700",
+  secondary: "bg-gray-600 text-white hover:bg-gray-700",
+  // danger: "bg-red-600 text-white hover:bg-red-700",
+  // success: "bg-green-600 text-white hover:bg-green-700",
+};
+
+const sizeStyles: Record<Size, string> = {
+  sm: "px-3 py-1 text-sm",
+  md: "px-4 py-2 text-base",
+  lg: "px-6 py-3 text-lg",
+};
+
+const Button: React.FC<ButtonProps> =({
+  
+  variant = "primary",
+  size = "md",
+  text = "Button",
+  className = "",
+  ...props
+}) =>{
   return (
-    <div className='bg-blue-500 rounded-sm'>
-      Button
-    </div>
+    <button  className={`rounded-md font-medium transition-all duration-200 
+        ${variantStyles[variant]} ${sizeStyles[size]} ${className}`}
+      {...props}>
+      {text}
+    </button>
   )
 }
 
