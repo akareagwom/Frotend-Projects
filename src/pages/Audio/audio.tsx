7ac59f516c8df21140c8b8576b156845e3e59826
akareagwom <akareazikang20@gmail.com>
2025-12-10 23:57:45 +0100

    header

--- src/pages/Audio/audio.tsx
+++ src/pages/Audio/audio.tsx
@@ -3,7 +3,9 @@
 function Audio() {
   return (
     <div className="bg-[#1C1E26] m-0 h-[100vh] w-full">
-      <div className="flex"></div>
+      <div className="flex justify-center items-center bg-[#2A2D36]">
+        <h1>Audio Recorder</h1>
+      </div>
     </div>
   )
 }
