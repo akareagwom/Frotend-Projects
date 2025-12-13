a12640925bb589f28f1906b53d68b04a4fbac93a
akareagwom <akareazikang20@gmail.com>
2025-12-13 20:51:01 +0100

    pause button

--- src/pages/Audio/audio.tsx
+++ src/pages/Audio/audio.tsx
@@ -3,8 +3,13 @@
 function Audio() {
   return (
     <div className="bg-[#1C1E26] m-0 h-[100vh] w-full">
-      <div className="flex justify-center items-center bg-[#2A2D36]">
+      <div className="flex justify-center flex-col items-center bg-[#2A2D36]">
         <h1>Audio Recorder</h1>
+        <div className="rounded-[50%] bg-[#3A3D47] p-1">
+          <div className="rounded-[50%] text-3xl p-4 bg-[#D64331]">
+            {/* <p>3:00</p> */}
+          </div>
+        </div>
       </div>
     </div>
   )
