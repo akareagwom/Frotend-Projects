289bb24708452c2a1a32f89ea677558e6e284510
akareagwom <akareazikang20@gmail.com>
2025-12-17 23:42:02 +0100

    controls added

--- src/pages/Audio/audio.tsx
+++ src/pages/Audio/audio.tsx
@@ -1,13 +1,22 @@
-// import React from 'react'
+import{useState} from 'react';
+import { FaPlay, FaPause } from "react-icons/fa";
 
 function Audio() {
+  const [control, setControl]= useState(false);
+
+  const handleControl=()=>{
+    setControl(!control)
+  }
+
   return (
     <div className="bg-[#1C1E26] m-0 h-[100vh] w-full">
       <div className="flex justify-center flex-col items-center bg-[#2A2D36]">
         <h1>Audio Recorder</h1>
-        <div className="rounded-[50%] bg-[#3A3D47] p-1">
+        <div onClick={handleControl} className="rounded-[50%] cursor-pointer bg-[#3A3D47] p-1">
           <div className="rounded-[50%] text-3xl p-4 bg-[#D64331]">
-            {/* <p>3:00</p> */}
+            {control?
+            <FaPause />:<FaPlay />
+            }
           </div>
         </div>
       </div>
